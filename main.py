c722b3b3948cfbb4ebac57df5918c8d96a475b0c
Carlos <carlos@2kpixelstudios.net>
2020-06-03 21:05:04 -0400

    Added for loop using range(...)

--- main.py
+++ main.py
@@ -9,9 +9,19 @@ import example_module
 
 print("Whoa, it just works??")
 print(example_module.test_method())
-
 print("\n")
+
 print(example_module.SOME_CONSTANT)
 example_module.SOME_CONSTANT = 34
 print(example_module.SOME_CONSTANT)
 print("But.. it's actually not a constant. Just don't go changing it like this..!")
+print("\n")
+
+name = input("What's your name? ")
+print("You entered " + name + "!")
+
+nameLength = len(name)
+for i in range(nameLength):
+	print("name[" + str(i) + "] = " + name[i])
+print("\n")
+
